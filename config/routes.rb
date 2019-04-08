fbd91108b32295afa71a5fc92404fd0aabc27de7
pooja mokariya <poojalm747@gmail.com>
2019-04-08 15:09:07 +0530

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  resources :templates
+  root :to => "templates#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
